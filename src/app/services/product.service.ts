2204d9d7969b6635168e022f91b995307bf06527
Guilherme <willfkrause@outlook.com>
2023-02-09 18:16:09 -0300

    Update product.service.ts

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -25,7 +25,7 @@ export class ProductService {
         {
           headers: new HttpHeaders({
             Authorization:
-              'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1aWQiOjE1ODQ0LCJpYXQiOjE2NjYxMjcwNzJ9.VZTfF9eXBWxD1Ub0Z-gfZK3ygDMgJqd-OFt6svkWly4',
+              '',
           }),
         }
       )
